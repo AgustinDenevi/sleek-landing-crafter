76241d4f15201a9f03ad0d3ebf4ddd6a008b52b2
AgusDenevi <agusdenevi3010@gmail.com>
2024-10-21 16:26:46 -0300

    pruebav tr

--- src/components/IconCloudSection.tsx
+++ src/components/IconCloudSection.tsx
@@ -5,6 +5,15 @@ import TextReveal from "@/components/magicui/text-reveal";
 
 
 
+export function TextRevealDemo() {
+  return (
+    <div className="z-10 flex min-h-64 items-center justify-center rounded-lg border bg-white dark:bg-black">
+      <TextReveal text="Magic UI will change the way you design." />
+    </div>
+  );
+}
+
+
 const Card = () => {
   return (
     <StyledWrapper>
