6d7ecac4e11700b310646051b4bac26635faa044
AgusDenevi <agusdenevi3010@gmail.com>
2024-10-21 12:37:43 -0300

    contenido tarjetas

--- src/components/IconCloudSection.tsx
+++ src/components/IconCloudSection.tsx
@@ -5,20 +5,29 @@ const Card = () => {
   return (
     <StyledWrapper>
       <div className="card">
-        <div className="card2"></div>
+        <div className="card2">
+           <h3>Get your</h3>
+           <h2>Custom Software</h2>
+        </div>
       </div>
       <div className="card">
-        <div className="card2"></div>
+        <div className="card2">
+           <h3>Get your</h3>
+           <h2>Custom Software</h2>
+        </div>
       </div>
       <div className="card">
-        <div className="card2"></div>
+        <div className="card2">
+           <h3>Get your</h3>
+           <h2>Custom Software</h2>
+        </div>
       </div>
     </StyledWrapper>
   );
 };
 
 const StyledWrapper = styled.div`
-  display: flex; /* Agregar display: flex para alinear las tarjetas horizontalmente */
+  display: flex; 
   justify-content:center;
 
   .card {
@@ -28,20 +37,20 @@ const StyledWrapper = styled.div`
     background-image: linear-gradient(163deg, #00ff75 0%, #3700ff 100%);
     border-radius: 20px;
     transition: all 0.3s;
-    margin: 10px; /* Espacio entre tarjetas, opcional */
+    margin: 20px; 
   }
 
   .card2 {
     width: 190px;
     height: 254px;
     background-color: #1a1a1a;
-    border-radius: 2px; /* Mantener el mismo border-radius */
+    border-radius: 2px; 
     transition: all 0.2s;
   }
 
   .card2:hover {
     transform: scale(0.98);
-    /* border-radius: 20px; Esto es opcional */
+    border-radius: 20px; 
   }
 
   .card:hover {
