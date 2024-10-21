07988fd75ec0a6b680f2919cbd07f0b9f8d3e471
AgusDenevi <agusdenevi3010@gmail.com>
2024-10-21 16:24:56 -0300

    text reveeal

--- src/components/magicui/text-reveal.tsx
+++ src/components/magicui/text-reveal.tsx
@@ -1,37 +1,73 @@
-import React, { useEffect, useRef } from 'react';
-import { motion, useInView, useAnimation } from 'framer-motion';
-
-interface Props {
+"use client";
+ 
+import { FC, ReactNode, useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+ 
+import { cn } from "@/lib/utils";
+ 
+interface TextRevealByWordProps {
   text: string;
   className?: string;
 }
-
-export const TextReveal: React.FC<Props> = ({ text, className = '' }) => {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
-  const mainControls = useAnimation();
-
-  useEffect(() => {
-    if (isInView) {
-      mainControls.start('visible');
-    }
-  }, [isInView]);
-
+ 
+export const TextRevealByWord: FC<TextRevealByWordProps> = ({
+  text,
+  className,
+}) => {
+  const targetRef = useRef<HTMLDivElement | null>(null);
+ 
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  });
+  const words = text.split(" ");
+ 
   return (
-    <div ref={ref} className={className}>
-      <motion.h2
-        variants={{
-          hidden: { opacity: 0, y: 75 },
-          visible: { opacity: 1, y: 0 },
-        }}
-        initial="hidden"
-        animate={mainControls}
-        transition={{ duration: 0.5, delay: 0.25 }}
+    <div ref={targetRef} className={cn("relative z-0 h-[200vh]", className)}>
+      <div
+        className={
+          "sticky top-0 mx-auto flex h-[50%] max-w-4xl items-center bg-transparent px-[1rem] py-[5rem]"
+        }
       >
-        {text}
-      </motion.h2>
+        <p
+          ref={targetRef}
+          className={
+            "flex flex-wrap p-5 text-2xl font-bold text-black/20 dark:text-white/20 md:p-8 md:text-3xl lg:p-10 lg:text-4xl xl:text-5xl"
+          }
+        >
+          {words.map((word, i) => {
+            const start = i / words.length;
+            const end = start + 1 / words.length;
+            return (
+              <Word key={i} progress={scrollYProgress} range={[start, end]}>
+                {word}
+              </Word>
+            );
+          })}
+        </p>
+      </div>
     </div>
   );
 };
-
-export default TextReveal;
+ 
+interface WordProps {
+  children: ReactNode;
+  progress: any;
+  range: [number, number];
+}
+ 
+const Word: FC<WordProps> = ({ children, progress, range }) => {
+  const opacity = useTransform(progress, range, [0, 1]);
+  return (
+    <span className="xl:lg-3 relative mx-1 lg:mx-2.5">
+      <span className={"absolute opacity-30"}>{children}</span>
+      <motion.span
+        style={{ opacity: opacity }}
+        className={"text-black dark:text-white"}
+      >
+        {children}
+      </motion.span>
+    </span>
+  );
+};
+ 
+export default TextRevealByWord;
