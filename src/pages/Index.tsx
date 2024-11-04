e164c737ff98790177abd9e58469a5d4d3945e82
AgustinCitti <aguscitti@gmail.com>
2024-11-04 14:15:31 -0300

    no se

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import Hero from '../components/Hero';
 import IconCloudSection from '../components/IconCloudSection';
 import PCard from '../components/AiSection';
-import PricingPlans from '../components/PricingPlans';
+
 import MembershipSection from '../components/MembershipSection';
 import Testimonials from '../components/Testimonials';
 import Footer from '../components/Footer';
-
 
 const Index = () => {
   return (
